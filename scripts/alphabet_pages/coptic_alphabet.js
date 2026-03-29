76fb0b61596eca9c74e6142429a32c3f26e8a90f
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2026-03-29 17:01:28 +0200

    Added bookshop header

--- scripts/alphabet_pages/coptic_alphabet.js
+++ scripts/alphabet_pages/coptic_alphabet.js
@@ -1,82 +1,233 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const coreLetters = [
-    { upper: "Ⲁ", lower: "ⲁ", name: "alfa", id: "coptic-alfa" },
-    { upper: "Ⲃ", lower: "ⲃ", name: "vida", id: "coptic-vida" },
-    { upper: "Ⲅ", lower: "ⲅ", name: "gamma", id: "coptic-gamma" },
-    { upper: "Ⲇ", lower: "ⲇ", name: "dalda", id: "coptic-dalda" },
-    { upper: "Ⲉ", lower: "ⲉ", name: "eie", id: "coptic-eie" },
-    { upper: "Ⲋ", lower: "ⲋ", name: "sou", id: "coptic-sou" },
-    { upper: "Ⲍ", lower: "ⲍ", name: "zata", id: "coptic-zata" },
-    { upper: "Ⲏ", lower: "ⲏ", name: "hate", id: "coptic-hate" },
-    { upper: "Ⲑ", lower: "ⲑ", name: "thethe", id: "coptic-thethe" },
-    { upper: "Ⲓ", lower: "ⲓ", name: "iauda", id: "coptic-iauda" },
-    { upper: "Ⲕ", lower: "ⲕ", name: "kapa", id: "coptic-kapa" },
-    { upper: "Ⲗ", lower: "ⲗ", name: "laula", id: "coptic-laula" },
-    { upper: "Ⲙ", lower: "ⲙ", name: "mi", id: "coptic-mi" },
-    { upper: "Ⲛ", lower: "ⲛ", name: "ni", id: "coptic-ni" },
-    { upper: "Ⲝ", lower: "ⲝ", name: "ksi", id: "coptic-ksi" },
-    { upper: "Ⲟ", lower: "ⲟ", name: "o", id: "coptic-o" },
-    { upper: "Ⲡ", lower: "ⲡ", name: "pi", id: "coptic-pi" },
-    { upper: "Ⲣ", lower: "ⲣ", name: "ro", id: "coptic-ro" },
-    { upper: "Ⲥ", lower: "ⲥ", name: "sima", id: "coptic-sima" },
-    { upper: "Ⲧ", lower: "ⲧ", name: "tau", id: "coptic-tau" },
-    { upper: "Ⲩ", lower: "ⲩ", name: "ua", id: "coptic-ua" },
-    { upper: "Ⲫ", lower: "ⲫ", name: "fi", id: "coptic-fi" },
-    { upper: "Ⲭ", lower: "ⲭ", name: "khi", id: "coptic-khi" },
-    { upper: "Ⲯ", lower: "ⲯ", name: "psi", id: "coptic-psi" },
-    { upper: "Ⲱ", lower: "ⲱ", name: "oou", id: "coptic-oou" },
+  const coreGroups = [
+    {
+      title: "Core Uppercase",
+      items: [
+        { letter: "Ⲁ", unicode: "Ⲁ", name: "alfa", id: "core-upper-alfa" },
+        { letter: "Ⲃ", unicode: "Ⲃ", name: "vida", id: "core-upper-vida" },
+        { letter: "Ⲅ", unicode: "Ⲅ", name: "gamma", id: "core-upper-gamma" },
+        { letter: "Ⲇ", unicode: "Ⲇ", name: "dalda", id: "core-upper-dalda" },
+        { letter: "Ⲉ", unicode: "Ⲉ", name: "eie", id: "core-upper-eie" },
+        { letter: "Ⲋ", unicode: "Ⲋ", name: "sou", id: "core-upper-sou" },
+        { letter: "Ⲍ", unicode: "Ⲍ", name: "zata", id: "core-upper-zata" },
+        { letter: "Ⲏ", unicode: "Ⲏ", name: "hate", id: "core-upper-hate" },
+        { letter: "Ⲑ", unicode: "Ⲑ", name: "thethe", id: "core-upper-thethe" },
+        { letter: "Ⲓ", unicode: "Ⲓ", name: "iauda", id: "core-upper-iauda" },
+        { letter: "Ⲕ", unicode: "Ⲕ", name: "kapa", id: "core-upper-kapa" },
+        { letter: "Ⲗ", unicode: "Ⲗ", name: "laula", id: "core-upper-laula" },
+        { letter: "Ⲙ", unicode: "Ⲙ", name: "mi", id: "core-upper-mi" },
+        { letter: "Ⲛ", unicode: "Ⲛ", name: "ni", id: "core-upper-ni" },
+        { letter: "Ⲝ", unicode: "Ⲝ", name: "ksi", id: "core-upper-ksi" },
+        { letter: "Ⲟ", unicode: "Ⲟ", name: "o", id: "core-upper-o" },
+        { letter: "Ⲡ", unicode: "Ⲡ", name: "pi", id: "core-upper-pi" },
+        { letter: "Ⲣ", unicode: "Ⲣ", name: "ro", id: "core-upper-ro" },
+        { letter: "Ⲥ", unicode: "Ⲥ", name: "sima", id: "core-upper-sima" },
+        { letter: "Ⲧ", unicode: "Ⲧ", name: "tau", id: "core-upper-tau" },
+        { letter: "Ⲩ", unicode: "Ⲩ", name: "ua", id: "core-upper-ua" },
+        { letter: "Ⲫ", unicode: "Ⲫ", name: "fi", id: "core-upper-fi" },
+        { letter: "Ⲭ", unicode: "Ⲭ", name: "khi", id: "core-upper-khi" },
+        { letter: "Ⲯ", unicode: "Ⲯ", name: "psi", id: "core-upper-psi" },
+        { letter: "Ⲱ", unicode: "Ⲱ", name: "oou", id: "core-upper-oou" },
+      ],
+    },
+    {
+      title: "Core Lowercase",
+      items: [
+        { letter: "ⲁ", unicode: "ⲁ", name: "alfa", id: "core-lower-alfa" },
+        { letter: "ⲃ", unicode: "ⲃ", name: "vida", id: "core-lower-vida" },
+        { letter: "ⲅ", unicode: "ⲅ", name: "gamma", id: "core-lower-gamma" },
+        { letter: "ⲇ", unicode: "ⲇ", name: "dalda", id: "core-lower-dalda" },
+        { letter: "ⲉ", unicode: "ⲉ", name: "eie", id: "core-lower-eie" },
+        { letter: "ⲋ", unicode: "ⲋ", name: "sou", id: "core-lower-sou" },
+        { letter: "ⲍ", unicode: "ⲍ", name: "zata", id: "core-lower-zata" },
+        { letter: "ⲏ", unicode: "ⲏ", name: "hate", id: "core-lower-hate" },
+        { letter: "ⲑ", unicode: "ⲑ", name: "thethe", id: "core-lower-thethe" },
+        { letter: "ⲓ", unicode: "ⲓ", name: "iauda", id: "core-lower-iauda" },
+        { letter: "ⲕ", unicode: "ⲕ", name: "kapa", id: "core-lower-kapa" },
+        { letter: "ⲗ", unicode: "ⲗ", name: "laula", id: "core-lower-laula" },
+        { letter: "ⲙ", unicode: "ⲙ", name: "mi", id: "core-lower-mi" },
+        { letter: "ⲛ", unicode: "ⲛ", name: "ni", id: "core-lower-ni" },
+        { letter: "ⲝ", unicode: "ⲝ", name: "ksi", id: "core-lower-ksi" },
+        { letter: "ⲟ", unicode: "ⲟ", name: "o", id: "core-lower-o" },
+        { letter: "ⲡ", unicode: "ⲡ", name: "pi", id: "core-lower-pi" },
+        { letter: "ⲣ", unicode: "ⲣ", name: "ro", id: "core-lower-ro" },
+        { letter: "ⲥ", unicode: "ⲥ", name: "sima", id: "core-lower-sima" },
+        { letter: "ⲧ", unicode: "ⲧ", name: "tau", id: "core-lower-tau" },
+        { letter: "ⲩ", unicode: "ⲩ", name: "ua", id: "core-lower-ua" },
+        { letter: "ⲫ", unicode: "ⲫ", name: "fi", id: "core-lower-fi" },
+        { letter: "ⲭ", unicode: "ⲭ", name: "khi", id: "core-lower-khi" },
+        { letter: "ⲯ", unicode: "ⲯ", name: "psi", id: "core-lower-psi" },
+        { letter: "ⲱ", unicode: "ⲱ", name: "oou", id: "core-lower-oou" },
+      ],
+    },
   ];
 
   const demoticLetters = [
-    { upper: "Ϣ", lower: "ϣ", name: "shei", id: "demotic-shei" },
-    { upper: "Ϥ", lower: "ϥ", name: "fei", id: "demotic-fei" },
-    { upper: "Ϧ", lower: "ϧ", name: "khei", id: "demotic-khei" },
-    { upper: "Ϩ", lower: "ϩ", name: "hori", id: "demotic-hori" },
-    { upper: "Ϫ", lower: "ϫ", name: "gangia", id: "demotic-gangia" },
-    { upper: "Ϭ", lower: "ϭ", name: "shima", id: "demotic-shima" },
-    { upper: "Ϯ", lower: "ϯ", name: "dei", id: "demotic-dei" },
+    { letter: "Ϣ", unicode: "Ϣ", name: "shei upper", id: "demotic-shei-upper" },
+    { letter: "ϣ", unicode: "ϣ", name: "shei lower", id: "demotic-shei-lower" },
+    { letter: "Ϥ", unicode: "Ϥ", name: "fei upper", id: "demotic-fei-upper" },
+    { letter: "ϥ", unicode: "ϥ", name: "fei lower", id: "demotic-fei-lower" },
+    { letter: "Ϧ", unicode: "Ϧ", name: "khei upper", id: "demotic-khei-upper" },
+    { letter: "ϧ", unicode: "ϧ", name: "khei lower", id: "demotic-khei-lower" },
+    { letter: "Ϩ", unicode: "Ϩ", name: "hori upper", id: "demotic-hori-upper" },
+    { letter: "ϩ", unicode: "ϩ", name: "hori lower", id: "demotic-hori-lower" },
+    {
+      letter: "Ϫ",
+      unicode: "Ϫ",
+      name: "gangia upper",
+      id: "demotic-gangia-upper",
+    },
+    {
+      letter: "ϫ",
+      unicode: "ϫ",
+      name: "gangia lower",
+      id: "demotic-gangia-lower",
+    },
+    {
+      letter: "Ϭ",
+      unicode: "Ϭ",
+      name: "shima upper",
+      id: "demotic-shima-upper",
+    },
+    {
+      letter: "ϭ",
+      unicode: "ϭ",
+      name: "shima lower",
+      id: "demotic-shima-lower",
+    },
+    { letter: "Ϯ", unicode: "Ϯ", name: "dei upper", id: "demotic-dei-upper" },
+    { letter: "ϯ", unicode: "ϯ", name: "dei lower", id: "demotic-dei-lower" },
   ];
 
   const historicLetters = [
-    { upper: "Ⲳ", lower: "ⲳ", name: "dialect-p alef", id: "hist-alef" },
-    { upper: "Ⲵ", lower: "ⲵ", name: "old coptic ain", id: "hist-ain" },
-    { upper: "Ⲷ", lower: "ⲷ", name: "cryptogrammic eie", id: "hist-eie" },
-    { upper: "Ⲹ", lower: "ⲹ", name: "dialect-p kapa", id: "hist-kapa" },
-    { upper: "Ⲻ", lower: "ⲻ", name: "dialect-p ni", id: "hist-ni" },
-    { upper: "Ⲽ", lower: "ⲽ", name: "cryptogrammic ni", id: "hist-crypt-ni" },
-    { upper: "Ⲿ", lower: "ⲿ", name: "old coptic oou", id: "hist-oou" },
-    { upper: "Ⳁ", lower: "ⳁ", name: "sampi", id: "hist-sampi" },
-    { upper: "Ⳃ", lower: "ⳃ", name: "crossed shei", id: "hist-crossed-shei" },
-    { upper: "Ⳅ", lower: "ⳅ", name: "old coptic shei", id: "hist-old-shei" },
-    { upper: "Ⳇ", lower: "ⳇ", name: "old coptic esh", id: "hist-esh" },
-    { upper: "Ⳉ", lower: "ⳉ", name: "akhmimic khei", id: "hist-akhmimic-khei" },
-    { upper: "Ⳋ", lower: "ⳋ", name: "dialect-p hori", id: "hist-hori" },
-    { upper: "Ⳍ", lower: "ⳍ", name: "old coptic hori", id: "hist-old-hori" },
-    { upper: "Ⳏ", lower: "ⳏ", name: "old coptic ha", id: "hist-ha" },
-    { upper: "Ⳑ", lower: "ⳑ", name: "l-shaped ha", id: "hist-lha" },
-    { upper: "Ⳓ", lower: "ⳓ", name: "old coptic hei", id: "hist-hei" },
-    { upper: "Ⳕ", lower: "ⳕ", name: "old coptic hat", id: "hist-hat" },
+    { letter: "Ⲳ", unicode: "Ⲳ", name: "alef upper", id: "hist-alef-upper" },
+    { letter: "ⲳ", unicode: "ⲳ", name: "alef lower", id: "hist-alef-lower" },
+    { letter: "Ⲵ", unicode: "Ⲵ", name: "ain upper", id: "hist-ain-upper" },
+    { letter: "ⲵ", unicode: "ⲵ", name: "ain lower", id: "hist-ain-lower" },
+    { letter: "Ⲷ", unicode: "Ⲷ", name: "eie upper", id: "hist-eie-upper" },
+    { letter: "ⲷ", unicode: "ⲷ", name: "eie lower", id: "hist-eie-lower" },
+    { letter: "Ⲹ", unicode: "Ⲹ", name: "kapa upper", id: "hist-kapa-upper" },
+    { letter: "ⲹ", unicode: "ⲹ", name: "kapa lower", id: "hist-kapa-lower" },
+    { letter: "Ⲻ", unicode: "Ⲻ", name: "ni upper", id: "hist-ni-upper" },
+    { letter: "ⲻ", unicode: "ⲻ", name: "ni lower", id: "hist-ni-lower" },
     {
-      upper: "Ⳗ",
-      lower: "ⳗ",
-      name: "old coptic gangia",
-      id: "hist-old-gangia",
-    },
-    { upper: "Ⳙ", lower: "ⳙ", name: "old coptic dja", id: "hist-dja" },
-    { upper: "Ⳛ", lower: "ⳛ", name: "old coptic shima", id: "hist-old-shima" },
-    {
-      upper: "Ⳬ",
-      lower: "ⳬ",
-      name: "cryptogrammic shei",
-      id: "hist-crypt-shei",
+      letter: "Ⲽ",
+      unicode: "Ⲽ",
+      name: "crypt ni upper",
+      id: "hist-crypt-ni-upper",
     },
     {
-      upper: "Ⳮ",
-      lower: "ⳮ",
-      name: "cryptogrammic gangia",
-      id: "hist-crypt-gangia",
+      letter: "ⲽ",
+      unicode: "ⲽ",
+      name: "crypt ni lower",
+      id: "hist-crypt-ni-lower",
     },
-    { upper: "Ⳳ", lower: "ⳳ", name: "bohairic khei", id: "hist-bohairic-khei" },
+    { letter: "Ⲿ", unicode: "Ⲿ", name: "oou upper", id: "hist-oou-upper" },
+    { letter: "ⲿ", unicode: "ⲿ", name: "oou lower", id: "hist-oou-lower" },
+    { letter: "Ⳁ", unicode: "Ⳁ", name: "sampi upper", id: "hist-sampi-upper" },
+    { letter: "ⳁ", unicode: "ⳁ", name: "sampi lower", id: "hist-sampi-lower" },
+    {
+      letter: "Ⳃ",
+      unicode: "Ⳃ",
+      name: "crossed shei upper",
+      id: "hist-cross-shei-upper",
+    },
+    {
+      letter: "ⳃ",
+      unicode: "ⳃ",
+      name: "crossed shei lower",
+      id: "hist-cross-shei-lower",
+    },
+    {
+      letter: "Ⳅ",
+      unicode: "Ⳅ",
+      name: "old shei upper",
+      id: "hist-old-shei-upper",
+    },
+    {
+      letter: "ⳅ",
+      unicode: "ⳅ",
+      name: "old shei lower",
+      id: "hist-old-shei-lower",
+    },
+    { letter: "Ⳇ", unicode: "Ⳇ", name: "esh upper", id: "hist-esh-upper" },
+    { letter: "ⳇ", unicode: "ⳇ", name: "esh lower", id: "hist-esh-lower" },
+    { letter: "Ⳉ", unicode: "Ⳉ", name: "khei upper", id: "hist-khei-upper" },
+    { letter: "ⳉ", unicode: "ⳉ", name: "khei lower", id: "hist-khei-lower" },
+    { letter: "Ⳋ", unicode: "Ⳋ", name: "hori upper", id: "hist-hori-upper" },
+    { letter: "ⳋ", unicode: "ⳋ", name: "hori lower", id: "hist-hori-lower" },
+    {
+      letter: "Ⳍ",
+      unicode: "Ⳍ",
+      name: "old hori upper",
+      id: "hist-old-hori-upper",
+    },
+    {
+      letter: "ⳍ",
+      unicode: "ⳍ",
+      name: "old hori lower",
+      id: "hist-old-hori-lower",
+    },
+    { letter: "Ⳏ", unicode: "Ⳏ", name: "ha upper", id: "hist-ha-upper" },
+    { letter: "ⳏ", unicode: "ⳏ", name: "ha lower", id: "hist-ha-lower" },
+    { letter: "Ⳑ", unicode: "Ⳑ", name: "l-ha upper", id: "hist-lha-upper" },
+    { letter: "ⳑ", unicode: "ⳑ", name: "l-ha lower", id: "hist-lha-lower" },
+    { letter: "Ⳓ", unicode: "Ⳓ", name: "hei upper", id: "hist-hei-upper" },
+    { letter: "ⳓ", unicode: "ⳓ", name: "hei lower", id: "hist-hei-lower" },
+    { letter: "Ⳕ", unicode: "Ⳕ", name: "hat upper", id: "hist-hat-upper" },
+    { letter: "ⳕ", unicode: "ⳕ", name: "hat lower", id: "hist-hat-lower" },
+    {
+      letter: "Ⳗ",
+      unicode: "Ⳗ",
+      name: "gangia upper",
+      id: "hist-gangia-upper",
+    },
+    {
+      letter: "ⳗ",
+      unicode: "ⳗ",
+      name: "gangia lower",
+      id: "hist-gangia-lower",
+    },
+    { letter: "Ⳙ", unicode: "Ⳙ", name: "dja upper", id: "hist-dja-upper" },
+    { letter: "ⳙ", unicode: "ⳙ", name: "dja lower", id: "hist-dja-lower" },
+    { letter: "Ⳛ", unicode: "Ⳛ", name: "shima upper", id: "hist-shima-upper" },
+    { letter: "ⳛ", unicode: "ⳛ", name: "shima lower", id: "hist-shima-lower" },
+    {
+      letter: "Ⳬ",
+      unicode: "Ⳬ",
+      name: "crypt shei upper",
+      id: "hist-crypt-shei-upper",
+    },
+    {
+      letter: "ⳬ",
+      unicode: "ⳬ",
+      name: "crypt shei lower",
+      id: "hist-crypt-shei-lower",
+    },
+    {
+      letter: "Ⳮ",
+      unicode: "Ⳮ",
+      name: "crypt gangia upper",
+      id: "hist-crypt-gangia-upper",
+    },
+    {
+      letter: "ⳮ",
+      unicode: "ⳮ",
+      name: "crypt gangia lower",
+      id: "hist-crypt-gangia-lower",
+    },
+    {
+      letter: "Ⳳ",
+      unicode: "Ⳳ",
+      name: "bohairic khei upper",
+      id: "hist-bohairic-khei-upper",
+    },
+    {
+      letter: "ⳳ",
+      unicode: "ⳳ",
+      name: "bohairic khei lower",
+      id: "hist-bohairic-khei-lower",
+    },
   ];
 
   const symbols = [
@@ -91,40 +242,25 @@ document.addEventListener("DOMContentLoaded", function () {
   ];
 
   const marksAndPunctuation = [
-    {
-      letter: "⳯",
-      unicode: "⳯",
-      name: "combining ni above",
-      id: "mark-ni-above",
-    },
-    {
-      letter: "⳰",
-      unicode: "⳰",
-      name: "combining spiritus asper",
-      id: "mark-asper",
-    },
-    {
-      letter: "⳱",
-      unicode: "⳱",
-      name: "combining spiritus lenis",
-      id: "mark-lenis",
-    },
+    { letter: "⳯", unicode: "⳯", name: "ni above", id: "mark-ni-above" },
+    { letter: "⳰", unicode: "⳰", name: "asper", id: "mark-asper" },
+    { letter: "⳱", unicode: "⳱", name: "lenis", id: "mark-lenis" },
     {
       letter: "⳹",
       unicode: "⳹",
-      name: "old nubian full stop",
+      name: "old nubian stop",
       id: "punct-old-nubian-stop",
     },
     {
       letter: "⳺",
       unicode: "⳺",
-      name: "direct question mark",
+      name: "direct question",
       id: "punct-direct-question",
     },
     {
       letter: "⳻",
       unicode: "⳻",
-      name: "indirect question mark",
+      name: "indirect question",
       id: "punct-indirect-question",
     },
     {
@@ -137,7 +273,7 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       letter: "⳿",
       unicode: "⳿",
-      name: "morphological divider",
+      name: "morph divider",
       id: "punct-morph-divider",
     },
   ];
@@ -170,35 +306,6 @@ document.addEventListener("DOMContentLoaded", function () {
     return key;
   }
 
-  function createLetterPairCard(item) {
-    const col = document.createElement("div");
-    col.className = "col-xl-2 col-lg-3 col-md-4 col-sm-6";
-
-    const key = document.createElement("div");
-    key.className = "alphabet-key";
-    key.id = item.id;
-
-    const letterSpan = document.createElement("span");
-    letterSpan.className = "letter";
-    letterSpan.innerText = `${item.upper} ${item.lower}`;
-
-    const nameSpan = document.createElement("span");
-    nameSpan.className = "name";
-    nameSpan.innerText = item.name;
-
-    key.appendChild(letterSpan);
-    key.appendChild(nameSpan);
-
-    key.dataset.unicode = `${item.upper} ${item.lower}`;
-    key.onclick = function (event) {
-      copyToClipboard(`${item.upper} ${item.lower}`);
-      showTooltip("Copied", event);
-    };
-
-    col.appendChild(key);
-    return col;
-  }
-
   function createKeys(containerId, items) {
     const container = document.getElementById(containerId);
     items.forEach((item) => {
@@ -206,10 +313,31 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  function createLetterPairs(containerId, items) {
+  function createFamilyCards(containerId, groups) {
     const container = document.getElementById(containerId);
-    items.forEach((item) => {
-      container.appendChild(createLetterPairCard(item));
+
+    groups.forEach((group) => {
+      const col = document.createElement("div");
+      col.className = "col-xl-6 col-lg-6 col-md-6";
+
+      const card = document.createElement("div");
+      card.className = "card alphabet-card h-100";
+
+      const header = document.createElement("div");
+      header.className = "card-header text-center";
+      header.innerText = group.title;
+
+      const body = document.createElement("div");
+      body.className = "card-body text-center alphabet-grid";
+
+      group.items.forEach((item) => {
+        body.appendChild(createKey(item));
+      });
+
+      card.appendChild(header);
+      card.appendChild(body);
+      col.appendChild(card);
+      container.appendChild(col);
     });
   }
 
@@ -264,9 +392,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  createLetterPairs("coptic-core-container", coreLetters);
-  createLetterPairs("coptic-demotic-container", demoticLetters);
-  createLetterPairs("coptic-historic-container", historicLetters);
+  createFamilyCards("coptic-core-container", coreGroups);
+  createKeys("coptic-demotic-container", demoticLetters);
+  createKeys("coptic-historic-container", historicLetters);
   createKeys("coptic-symbols-container", symbols);
   createKeys("coptic-marks-container", marksAndPunctuation);
 });
